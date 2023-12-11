93d4e0e8a88f33e41feac1e96fe9e1fd5772446c
vellynnatalie <vellyn.natalie11@gmail.com>
2023-12-11 08:12:45 +0700

    search news

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,6 +4,6 @@ const categoryController = require('../controllers/categoryController.js');
 
 router.get('/category', categoryController.getAllCategory); //panggil semua category
 router.get('/category/:id', categoryController.getCatById); //panggil category sesuai id
-router.get('/category/:id/news', categoryController.getCatById); //panggil semua news sesuai id cat
+router.get('/category/:id/news', categoryController.getNewsByCatId); //panggil semua news sesuai id cat
 
 module.exports = router;
